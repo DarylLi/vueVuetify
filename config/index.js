d64096acfe7edef62efa6cecda4d681d81f451c9
李浩天 <haotian.li@taojiji.com>
2019-05-20 14:19:00 +0800

    env update

--- config/index.js
+++ config/index.js
@@ -6,7 +6,7 @@ const path = require('path')
 
 module.exports = {
   dev: {
-    port: 8080,
+    port: 8088,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
